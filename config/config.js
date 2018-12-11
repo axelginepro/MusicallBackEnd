fa7997478f49313af4e20d7334d49b64737e554f
Axel GINEPRO <axelginepro@gmail.com>
2018-12-11 10:20:58 +0100

    day2

---
+++ config/config.js
@@ -0,0 +1,7 @@
+const config = module.exports = {
+    url: 'mongodb://musicall:Rafnicludax69@ds127624.mlab.com:27624/musicall',
+    options: {
+        connectTimeoutMS: 5000,
+        useNewUrlParser: true
+    }
+};
